3a5f9f95ba67bc8a28c1e002a05514934fe6ce78
vasanth8871 <156489538+vasanth8871@users.noreply.github.com>
2024-02-12 13:10:38 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     = "linuxkey"
+  default     = "dockerkey"
 }
 
 variable "instance_type" {
